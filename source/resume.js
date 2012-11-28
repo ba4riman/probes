2611cdb951e6a5e94f075baac8c7d1bc231e0231
ba4riman <ba4riman@gmail.com>
2012-11-28 19:43:25 +0200

    All answers on question

--- source/resume.js
+++ source/resume.js
@@ -5,19 +5,17 @@ $(document).ready(function(){
 		$(this).data("padding-bottom", $(this).css("padding-bottom"));
 	});
 	$(".block-question__lists > div:not(:first)").hide();
+	$(".block-question__lists > li").addClass("active");
 	$(".block-question__lists > li").click(function(){
-		/*$(".block-question__lists > div").hide();
-		$(this).next().show();*/
 		$(".block-question__lists > div:visible").animate({
-			height: 0,
-			"padding-top": 0,
-			"padding-bottom": 0
-		}, 1000, function(){$(this).hide()});
-		box = $(this).next().show();
-		$(box).animate({
-			height: $(box).data("height"),
-			"padding-top": $(box).data("padding-top"),
-			"padding-bottom": $(box).data("padding-bottom")
+			height: "toggle",
+			"padding-top": "toggle",
+			"padding-bottom": "toggle"
+		}, 1000);
+		$(this).next().animate({
+			height: "toggle",
+			"padding-top": "toggle",
+			"padding-bottom": "toggle"
 		}, 1000);
 	});
 });
