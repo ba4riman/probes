ff5bb65af9ee249ab3a87421a3a3f9ae4f258792
ba4riman <ba4riman@gmail.com>
2012-11-29 23:13:35 +0200

    Edit styles and animations

--- source/resume.js
+++ source/resume.js
@@ -1,21 +1,13 @@
 $(document).ready(function(){
-	$(".block-question__lists > div").each(function(){
-		$(this).data("height", $(this).height());
-		$(this).data("padding-top", $(this).css("padding-top"));
-		$(this).data("padding-bottom", $(this).css("padding-bottom"));
-	});
 	$(".block-question__lists > div:not(:first)").hide();
-	$(".block-question__lists > li").addClass("active");
 	$(".block-question__lists > li").click(function(){
 		$(".block-question__lists > div:visible").animate({
-			height: "toggle",
-			"padding-top": "toggle",
-			"padding-bottom": "toggle"
-		}, 1000);
+			height: "hide",
+			padding: "hide",
+		}, 500);
 		$(this).next().animate({
-			height: "toggle",
-			"padding-top": "toggle",
-			"padding-bottom": "toggle"
-		}, 1000);
+			height: "show",
+			padding: "show",
+		}, 500);
 	});
 });
